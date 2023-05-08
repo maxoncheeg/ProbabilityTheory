c8fe7ebe98d4861e454e4fb14e0a24a19d36fb1d
Semenov Maxim <84966651+maxoncheeg@users.noreply.github.com>
2023-05-08 22:53:45 +0700

    Confidence interval (#7)
    * Complete refactoring of the project. Structuring.
    
    * Added Confidence Intervals
    
    * Implementation of construction of intervals has been completed
    
    * Redactored

---
+++ ProbabilityTheory/Classes/ConfidenceInterval.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Linq;
+
+namespace ProbabilityTheory.Classes
+{
+	internal class ConfidenceInterval
+	{
+		static Application excel = new Application();
+
+		public double LeftBoundary { get; private set; }
+		public double RightBoundary { get; private set; }
+		public double Gamma { get; private set; }
+		public Selection Selection { get; private set; }
+
+		private ConfidenceInterval(double LeftBoundary, double RightBoundary)
+		{
+			this.LeftBoundary = LeftBoundary;
+			this.RightBoundary = RightBoundary;
+		}
+
+		public static ConfidenceInterval Expectation(Selection selection, double gamma)
+		{
+			double alpha = 1f - gamma,
+				t = excel.WorksheetFunction.TInv(alpha / 2, selection.Values.Count - 1),
+				avgX = selection.Values.Average(),
+				s = Math.Sqrt(selection.Values.Sum((x) => Math.Pow(x - avgX, 2)) / (selection.Values.Count - 1)),
+				delta = t * s / Math.Sqrt(selection.Values.Count);
+
+			return new ConfidenceInterval(avgX - delta, avgX + delta) { Selection = selection, Gamma = gamma };
+		}
+
+		public static ConfidenceInterval ExpectationByVariance(Selection selection, double gamma, double variance)
+		{
+			double z = excel.WorksheetFunction.Norm_S_Inv((1f + gamma) / 2),
+				delta = z * Math.Sqrt(variance / selection.Values.Count),
+				avgX = selection.Values.Average();
+
+			return new ConfidenceInterval(avgX - delta, avgX + delta) { Selection = selection, Gamma = gamma };
+		}
+
+		public static ConfidenceInterval Variance(Selection selection, double gamma)
+		{
+			double alpha = 1f - gamma,
+				xi1 = excel.WorksheetFunction.ChiSq_Inv(1f - alpha / 2, selection.Values.Count - 1),
+				xi2 = excel.WorksheetFunction.ChiSq_Inv(alpha / 2, selection.Values.Count - 1),
+				avgX = selection.Values.Average(),
+				sCube = selection.Values.Sum((x) => Math.Pow(x - avgX, 2)) / (selection.Values.Count - 1);
+
+			return new ConfidenceInterval(sCube * (selection.Values.Count - 1) / xi1, sCube * (selection.Values.Count - 1) / xi2) { Selection = selection, Gamma = gamma };
+		}
+
+		public static ConfidenceInterval VarianceByExpectation(Selection selection, double gamma, double expectation)
+		{
+			double alpha = 1f - gamma,
+				xi1 = excel.WorksheetFunction.ChiSq_Inv(1f - alpha / 2, selection.Values.Count - 1),
+				xi2 = excel.WorksheetFunction.ChiSq_Inv(alpha / 2, selection.Values.Count - 1),
+				avgX = selection.Values.Average(),
+				variance = selection.Values.Sum((x) => Math.Pow(x - expectation, 2)) / selection.Values.Count;
+
+			return new ConfidenceInterval(variance* selection.Values.Count / xi1, variance * selection.Values.Count / xi2) { Selection = selection, Gamma = gamma };
+		}
+	}
+}
---
+++ ProbabilityTheory/Forms/FormIntervals.cs
@@ -0,0 +1,80 @@
+﻿using ProbabilityTheory.Classes;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ProbabilityTheory.Forms
+{
+	public partial class FormIntervals : Form
+	{
+		Microsoft.Office.Interop.Excel.Application _application;
+		public FormIntervals()
+		{
+			InitializeComponent();
+
+			_application = new Microsoft.Office.Interop.Excel.Application();
+
+			buttonCalculate.Click += OnCalculateClick;
+			Load += FormIntervals_Load;
+		}
+
+		private void FormIntervals_Load(object sender, EventArgs e)
+		{
+			FormPlacer.ToScreenCenter(this);
+		}
+
+		private string ConvertToString(ConfidenceInterval interval) => $"N={interval.Selection.Values.Count} Gamma={interval.Gamma} ({Math.Round(interval.LeftBoundary, 2)}; {Math.Round(interval.RightBoundary, 2)})";
+
+		private void OnCalculateClick(object sender, EventArgs e)
+		{
+			DistributionHistogramBuilder builderB = DistributionHistogramBuilder.Create(chartHistogramBig.Series[0]);
+			DistributionHistogramBuilder builderS = DistributionHistogramBuilder.Create(chartHistogramSmall.Series[0]);
+
+			double deviation = (double)numericUpDownDeviation.Value,
+				expectation = (double)numericUpDownExpectation.Value;
+			Selection selectionBig = Selection.GetNormalSelection(500, 24, expectation, deviation);
+			Selection selectionSmall = Selection.GetNormalSelection(50, 24, expectation, deviation);
+
+			builderB.BuildHistogram(selectionBig, (int)(1 + 3.322 * Math.Log(selectionBig.Values.Count)));
+			builderS.BuildHistogram(selectionSmall, (int)(1 + 3.322 * Math.Log(selectionSmall.Values.Count)));
+
+			chartHistogramBig.Series[0].Name = "N = 500";
+			chartHistogramSmall.Series[0].Name = "N = 50";
+
+			using (StreamWriter writer = new StreamWriter("confidence-intervals.txt", false))
+			{
+				writer.WriteLine($"m={(double)numericUpDownExpectation.Value}, d={(double)numericUpDownDeviation.Value}");
+
+				writer.WriteLine("Доверительные интервалы мат. ожидания");
+				writer.WriteLine("При известной дисперсии");
+				writer.WriteLine(ConvertToString(ConfidenceInterval.ExpectationByVariance(selectionBig, 0.95, deviation * deviation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.ExpectationByVariance(selectionBig, 0.85, deviation * deviation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.ExpectationByVariance(selectionSmall, 0.95, deviation * deviation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.ExpectationByVariance(selectionSmall, 0.85, deviation * deviation)));
+
+				writer.WriteLine("При неизвестной дисперсии");
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Expectation(selectionBig, 0.95)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Expectation(selectionBig, 0.85)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Expectation(selectionSmall, 0.95)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Expectation(selectionSmall, 0.85)));
+
+				writer.WriteLine("");
+				writer.WriteLine("Доверительные интервалы дисперсии");
+				writer.WriteLine("При известном мат. ожидании");
+				writer.WriteLine(ConvertToString(ConfidenceInterval.VarianceByExpectation(selectionBig, 0.95, expectation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.VarianceByExpectation(selectionBig, 0.85, expectation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.VarianceByExpectation(selectionSmall, 0.95, expectation)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.VarianceByExpectation(selectionSmall, 0.85, expectation)));
+
+				writer.WriteLine("При неизвестном мат. ожидании");
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Variance(selectionBig, 0.95)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Variance(selectionBig, 0.85)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Variance(selectionSmall, 0.95)));
+				writer.WriteLine(ConvertToString(ConfidenceInterval.Variance(selectionSmall, 0.85)));
+
+			}
+
+			System.Diagnostics.Process.Start("confidence-intervals.txt");
+		}
+	}
+}
